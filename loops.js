47533a917d4d8a0dc1c2fc63a28c81ce4e32317c
Rob Whiting <rob.t.whiting@gmail.com>
2018-06-27 04:29:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5
   }
   do {
-    console.log(--countdown);
+    console.log(--array);
   } while (array.length > 0 && maybeTrue());
   return array;
 }
